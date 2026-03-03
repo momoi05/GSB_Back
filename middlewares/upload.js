3d7ce8d50925d834277ba73da0890ee044a7ceb1
momoi05 <tetsuya.satsuki.06@gmail.com>
2026-03-03 11:26:05 +0100

    pdf accept

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const storage = multer.memoryStorage();
 
 
-// Upload method that stores image files only and limits file size to 5MB
+// Upload method that stores image files (et éventuellement PDF) et limite la taille à 5MB
 const upload = multer({
     storage: storage,
     limits: {
@@ -10,9 +10,13 @@ const upload = multer({
     },
 
     fileFilter: (req, file, cb) => {
-        // Accept images only
-        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-            return cb(new Error('Only image files are allowed!'), false);
+        // Accepter uniquement les images OU les PDF
+        // plus robuste : se baser sur le mimetype plutôt que le nom de fichier
+        if (
+            !file.mimetype.startsWith('image/') && // toutes les images
+            file.mimetype !== 'application/pdf'     // et les PDF
+        ) {
+            return cb(new Error('Only image or PDF files are allowed!'), false);
         }
         cb(null, true);
     }
